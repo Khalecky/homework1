fad335db47efb7cdf3b85864c4ab788f3c72df0c
roman.kh <halecky@ya.ru>
2019-09-30 14:59:33 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include "my_constants.h"
+#include "version.h"
 
 using namespace std;
 
 int main()
 {
-    cout << OUTPUT_TEXT << endl << (OUTPUT_TEXT != "Hello, world!");
+    cout << "build " << PROJECT_VERSION_PATCH << endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
--- tests/test_module.cpp
+++ tests/test_module.cpp
@@ -1,17 +1,14 @@
-#define BOOST_TEST_DYN_LINK
+//#define BOOST_TEST_DYN_LINK   //2 variants: 1 - define here; 2- like compile option is CMakeLists
 #define BOOST_TEST_MODULE TestHomework1
 
 #include <boost/test/unit_test.hpp>
-#include "../my_constants.h"
+#include "../version.h"
 
 BOOST_AUTO_TEST_SUITE(TestHomework1)
 
-BOOST_AUTO_TEST_CASE(test_output)
+BOOST_AUTO_TEST_CASE(test_version)
 {
-    int i = 0;
-    BOOST_CHECK(i == 1);
-    BOOST_CHECK(OUTPUT_TEXT != "Hello, world!");
-
+    BOOST_CHECK(PROJECT_VERSION_PATCH > 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
